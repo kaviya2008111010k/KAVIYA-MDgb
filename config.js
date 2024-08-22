de4e896a062d3047686fbfc63c16489ba81446ed
Kaviya200011 <Kavishkamadushan268@gmail.com>
2024-08-23 01:06:19 +0530

    Update config.js

--- config.js
+++ config.js
@@ -8,4 +8,5 @@ module.exports = {
 SESSION_ID: process.env.SESSION_ID,
 ALIVE_IMG: process.env.ALIVE_IMG || "https://telegra.ph/file/bb6fdbbaa445ee722502a.jpg",
 ALIVE_MSG: process.env.ALIVE_MSG || "*I AM ALIVE NOW 😼💯*\n\n*Hey,KAVIYA-MD USER 💀⚡*\n\n*Sapport us ⚔️*\n\n*https://wa.me/+94752689596?text=*Hey_KAVIYA-MD-OWNER_😼*",
+AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
 };
