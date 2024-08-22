10c5e24556c338500bf533cd98290637a30f219d
Kaviya200011 <Kavishkamadushan268@gmail.com>
2024-08-22 20:56:06 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,5 +7,5 @@ function convertToBool(text, fault = 'true') {
 module.exports = {
 SESSION_ID: process.env.SESSION_ID,
 ALIVE_IMG: process.env.ALIVE_IMG || "https://telegra.ph/file/bb6fdbbaa445ee722502a.jpg",
-ALIVE_MSG: process.env.ALIVE_MSG || "𝗜 𝗔𝗠 𝗔𝗟𝗜𝗩𝗘 𝗡𝗢𝗪 😼💯\n\n*Hey,KAVIYA-MD USER 💀⚡*\n\n*Sapport us ⚔️*\n\n*https://wa.me/+94752689596?text=*Hey_KAVIYA-MD-OWNER_😼*",
+ALIVE_MSG: process.env.ALIVE_MSG || "*I AM ALIVE NOW 😼💯*\n\n*Hey,KAVIYA-MD USER 💀⚡*\n\n*Sapport us ⚔️*\n\n*https://wa.me/+94752689596?text=*Hey_KAVIYA-MD-OWNER_😼*",
 };
