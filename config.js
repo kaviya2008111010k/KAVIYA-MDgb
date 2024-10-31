95f7be687dc68e7963428407db5490d36222f148
Avishka_X <avishkathuruwiman08@gmail.com>
2024-10-31 16:12:19 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,6 +5,6 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "1CcmBDgQ#9RIPB_0cPprHXfzfc4AaC6Av0E10eAfCc-FJOitw9zs",
+SESSION_ID: process.env.SESSION_ID || "qbgBmLib#OSFf9T5ezP3LyOfzZZev7M2UnAFKOUvtciK6Cg0qUek",
 MONGODB: process.env.MONGODB || "mongodb://mongo:wwJAJJCCBPlcFeqNHXSoCjwyYiUxXkLh@junction.proxy.rlwy.net:29356",
 };
