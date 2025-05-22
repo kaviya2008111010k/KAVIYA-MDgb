12f7c1c473cd754014e370a5c0c8a0ffec97c023
kaviya2008111010k <Kavishkamadushan9596@gmail.com>
2025-05-22 07:59:01 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,6 +5,6 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "qbgBmLib#OSFf9T5ezP3LyOfzZZev7M2UnAFKOUvtciK6Cg0qUek",
+SESSION_ID: process.env.SESSION_ID || "kZZkCDiA#aT6l-VBX4EevQ8FD92LsYQLugeSs_d9yj6HhFSCsftI",
 MONGODB: process.env.MONGODB || "mongodb://mongo:wwJAJJCCBPlcFeqNHXSoCjwyYiUxXkLh@junction.proxy.rlwy.net:29356",
 };
